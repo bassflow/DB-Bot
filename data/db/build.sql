ec2837dc5c0ce4ae63f9e15e9c7ef9fdfc1e9b40
bassflow <78425120+bassflow@users.noreply.github.com>
2021-02-20 16:12:45 +0100

    Pre Alpha

---
+++ data/db/build.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS guilds (
+    GuildID integer PRIMARY KEY, 
+    Prefix text DEFAULT "+"
+);
+
+CREATE TABLE IF NOT EXISTS exp (
+    UserId integer PRIMARY KEY,
+    XP integer DEFAULT 0,
+    Level integer DEFAULT 0,
+    XPLock text DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS mutes (
+    UserID integer PRIMARY KEY, 
+    RoleIDs text,
+    EndTime text
+);
+
+CREATE TABLE IF NOT EXISTS starboard (
+    RootMessage ID integer PRIMARY KEY,
+    Starmessage integer, 
+    Stars integer Default 1
+);
